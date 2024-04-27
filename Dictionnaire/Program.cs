6c24d924a852a7d6fc3667e9a0669eb1674cc022
Mederic <kottomouyema.m@gmail.com>
2024-04-28 01:14:15 +0200

    Quête Dictionnaire

---
+++ Dictionnaire/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Dictionnaire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, List<string>> dictionnaire = new Dictionary<char, List<string>>();
+
+            dictionnaire['a'] = new List<string> { "Anaïs", "Année", "Agir" };
+            dictionnaire['b'] = new List<string> { "Béatrice", "Bulle", "Boire" };
+            dictionnaire['c'] = new List<string> { "Charles", "Chemise", "Chercher" };
+
+            Console.WriteLine("Dictionnaire avant modifications");
+            Console.WriteLine();
+            foreach (var item in dictionnaire)
+            {
+                Console.WriteLine(item.Key);
+            }
+
+            dictionnaire.Remove('c');
+
+            Console.WriteLine();
+            Console.WriteLine("Dictionnaire après modifications");
+            Console.WriteLine();
+            foreach (var item in dictionnaire)
+            {
+                Console.WriteLine(item.Key);  
+            }
+        }
+    }
+}
